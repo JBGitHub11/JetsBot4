365278c1da0082696b5b773535a753820dc0f5f4
JB <45403745+JBGitHub11@users.noreply.github.com>
2023-05-20 09:39:58 -0400

    refactoring

--- event_checks.py
+++ event_checks.py
@@ -5,72 +5,54 @@ import codecs
 
 class EventChecks:
     def __init__(self):
-        with codecs.open('config.ini', 'r', encoding='utf-8') as f:
+        with codecs.open("config.ini", "r", encoding="utf-8") as f:
             config = configparser.ConfigParser()
             config.read_file(f)
 
-        self.gg_message_list = [v for k, v in config.items('gg_messages')]
-        self.gg_authors = {}
-        self.gg_mention_window = 300  # 5 minutes
-        self.gg_message_cooldown = 1200  # 20 minutes
-        self.last_gg_response_time = 0  
+        self.gg_messages = [v for k, v in config.items("gg_messages")]
+        self.gg_mentions = {}
+        self.gg_mention_window = 60 * 5
+        self.gg_message_cooldown = 60 * 20
+        self.gg_last_message_time = 0
+        self.gg_user_threshold = 3
 
-        self.safety_meeting_messages = [v for k, v in config.items('safety_meeting_messages')]
-        self.safety_meeting_mentions = {}
-        self.last_safety_message_time = 0
-        self.safety_message_cooldown = 60 * 20  # 20 minutes
-        self.safety_mention_window = 60 * 5  # 5 minutes
+        self.safety_messages = [v for k, v in config.items("safety_meeting_messages")]
+        self.safety_mentions = {}
+        self.safety_mention_window = 60 * 5
+        self.safety_message_cooldown = 60 * 20
+        self.safety_last_message_time = 0
+        self.safety_user_threshold = 3
 
-        # Slava tracking
         self.slava_messages = [v for v in config["slava_messages"].values()]
         self.slava_mentions = {}
-        self.last_slava_message_time = 0
-        self.slava_message_cooldown = 60 * 20  # Twenty minutes in seconds
-        self.slava_mention_window = 2 * 60  # Two minutes in seconds
+        self.slava_mention_window = 2 * 60
+        self.slava_message_cooldown = 60 * 20
+        self.slava_last_message_time = 0
+        self.slava_user_threshold = 3
 
     def check_gg_event(self, author_name, message, youtube_chat, live_chat_id):
-        if message.lower() == 'gg':
-            self.gg_authors[author_name] = time.time()
-            self.gg_authors = {author: timestamp for author, timestamp in self.gg_authors.items() if time.time() - timestamp <= self.gg_mention_window}
-
-            if len(self.gg_authors) >= 1 and time.time() - self.last_gg_response_time > self.gg_message_cooldown:
-                random_message = random.choice(self.gg_message_list)
-                youtube_chat.send_live_chat_message(live_chat_id, random_message)
-                self.last_gg_response_time = time.time()
-                self.gg_authors.clear()
+        if message.lower() == "gg":
+            self.gg_mentions[author_name] = time.time()
+            self.gg_mentions = {author: timestamp for author, timestamp in self.gg_mentions.items() if time.time() - timestamp <= self.gg_mention_window}
+            if len(self.gg_mentions) >= self.gg_user_threshold and time.time() - self.gg_last_message_time > self.gg_message_cooldown:
+                youtube_chat.send_live_chat_message(live_chat_id, random.choice(self.gg_messages))
+                self.gg_last_message_time = time.time()
+                self.gg_mentions.clear()
 
     def check_safety_event(self, author_name, message, youtube_chat, live_chat_id):
-        if ("safety meeting" in message.lower() or ":_safety:" in message.lower()) and time.time() - self.last_safety_message_time > self.safety_message_cooldown:
-            self.safety_meeting_mentions[author_name] = time.time()
-            self.safety_meeting_mentions = {user: t for user, t in self.safety_meeting_mentions.items() if time.time() - t <= self.safety_mention_window}
-
-            if len(self.safety_meeting_mentions) >= 1:
-                random_message = random.choice(self.safety_meeting_messages)
-                youtube_chat.send_live_chat_message(live_chat_id, random_message)
-                self.last_safety_message_time = time.time()
-                self.safety_meeting_mentions.clear()
+        if "safety meeting" in message.lower() or ":_safety:" in message.lower() and time.time() - self.safety_last_message_time > self.safety_message_cooldown:
+            self.safety_mentions[author_name] = time.time()
+            self.safety_mentions = {user: t for user, t in self.safety_mentions.items() if time.time() - t <= self.safety_mention_window}
+            if len(self.safety_mentions) >= self.safety_user_threshold:
+                youtube_chat.send_live_chat_message(live_chat_id, random.choice(self.safety_messages))
+                self.safety_last_message_time = time.time()
+                self.safety_mentions.clear()
 
     def check_slava_event(self, author_name, message, youtube_chat, live_chat_id):
-        if ("slava ukraini" in message.lower() or "slava ukraine" in message.lower()) and time.time() - self.last_slava_message_time > self.slava_message_cooldown:
-            if (
-                author_name not in self.slava_mentions
-                or time.time() - self.slava_mentions[author_name]
-                > self.slava_mention_window
-            ):
-                self.slava_mentions[author_name] = time.time()  
-            if (
-                len(
-                    [
-                        user
-                        for user, mention_time in self.slava_mentions.items()
-                        if time.time() - mention_time
-                        <= self.slava_mention_window
-                    ]
-                )
-                >= 1
-            ):
-                random_message = random.choice(self.slava_messages)
-                youtube_chat.send_live_chat_message(live_chat_id, random_message)
-                self.last_slava_message_time = time.time()
+        if "slava ukraini" in message.lower() or "slava ukraine" in message.lower() and time.time() - self.slava_last_message_time > self.slava_message_cooldown:
+            if author_name not in self.slava_mentions or time.time() - self.slava_mentions[author_name] > self.slava_mention_window:
+                self.slava_mentions[author_name] = time.time()
+            if len([user for user, mention_time in self.slava_mentions.items() if time.time() - mention_time <= self.slava_mention_window]) >= self.slava_user_threshold:
+                youtube_chat.send_live_chat_message(live_chat_id, random.choice(self.slava_messages))
+                self.slava_last_message_time = time.time()
                 self.slava_mentions.clear()
-
